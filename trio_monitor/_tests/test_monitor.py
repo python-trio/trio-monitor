53f369a845b14bbb7dac43f376abe27ff648cd8f
Zach Thompson <zach@allotropic.com>
2019-08-09 21:47:32 -0400

    Use nursery block instead of fixture

--- trio_monitor/_tests/test_monitor.py
+++ trio_monitor/_tests/test_monitor.py
@@ -3,12 +3,13 @@ from trio_monitor.monitor import Monitor
 from trio.testing import open_stream_to_socket_listener
 
 
-async def test_monitor_connect(nursery):
-    m = Monitor()
-    listeners = await nursery.start(trio.serve_tcp, m.listen_on_stream, 0)
-    cli = await open_stream_to_socket_listener(listeners[0])
-    greeting = await cli.receive_some()
-    assert greeting[-6:] == b"trio> "
-    await cli.send_all(b"ps\r\n")
-    ps = await cli.receive_some()
-    assert b"trio.serve_tcp" in ps
+async def test_monitor_connect():
+    async with trio.open_nursery() as nursery:
+        m = Monitor()
+        listeners = await nursery.start(trio.serve_tcp, m.listen_on_stream, 0)
+        cli = await open_stream_to_socket_listener(listeners[0])
+        greeting = await cli.receive_some()
+        assert greeting[-6:] == b"trio> "
+        await cli.send_all(b"ps\r\n")
+        ps = await cli.receive_some()
+        assert b"trio.serve_tcp" in ps
