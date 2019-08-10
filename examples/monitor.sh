4028a0bc011f0c6b9415180760a14264c8b4db6f
Zach Thompson <zach@allotropic.com>
2019-08-09 21:26:35 -0400

    Upgrade to Trio 0.12.x
    The monitor code was out of date and this patch gets it working again.

---
+++ examples/monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python -m trio_monitor.monitor -a localhost -p 8899
+## or:
+# telnet localhost 8899
---
+++ trio_monitor/_tests/test_monitor.py
@@ -0,0 +1,14 @@
+import trio
+from trio_monitor.monitor import Monitor
+from trio.testing import open_stream_to_socket_listener
+
+
+async def test_monitor_connect(nursery):
+    m = Monitor()
+    listeners = await nursery.start(trio.serve_tcp, m.listen_on_stream, 0)
+    cli = await open_stream_to_socket_listener(listeners[0])
+    greeting = await cli.receive_some()
+    assert greeting[-6:] == b"trio> "
+    await cli.send_all(b"ps\r\n")
+    ps = await cli.receive_some()
+    assert b"trio.serve_tcp" in ps
