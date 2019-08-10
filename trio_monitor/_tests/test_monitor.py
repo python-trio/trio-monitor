849f9b742e8415ac25228ac940cbedadf31d0bd8
Zach Thompson <zach@allotropic.com>
2019-08-10 11:12:39 -0400

    Read from stream until match is found
    A travis-ci test fails in Python 3.5.0, possibly because the test
    assumes that receive_some() will return the entire stream buffer in
    one call (if it's small).  Rewrote the test without that assumption.

--- trio_monitor/_tests/test_monitor.py
+++ trio_monitor/_tests/test_monitor.py
@@ -7,10 +7,19 @@ async def test_monitor_connect():
     async with trio.open_nursery() as nursery:
         m = Monitor()
         listeners = await nursery.start(trio.serve_tcp, m.listen_on_stream, 0)
+
+        greeting = b" " * 6
         cli = await open_stream_to_socket_listener(listeners[0])
-        greeting = await cli.receive_some()
+        with trio.move_on_after(1):
+            while greeting[-6:] != b"trio> ":
+                greeting += await cli.receive_some(4)
         assert greeting[-6:] == b"trio> "
+
+        ps = b" " * 14
         await cli.send_all(b"ps\r\n")
-        ps = await cli.receive_some()
+        with trio.move_on_after(1):
+            while b"trio.serve_tcp" not in ps:
+                ps += await cli.receive_some(4)
         assert b"trio.serve_tcp" in ps
+
         nursery.cancel_scope.cancel()
